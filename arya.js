7a13d16a2bf5a6fe5ab5a8d5dbbcb5dd13b83388
kweku okyere Kankam <kweku360@gmail.com>
2014-09-14 16:05:08 +0000

    paging done - just a few tweaks left nxt is options

--- arya.js
+++ arya.js
@@ -18,42 +18,63 @@ Version : 0.1
             var slidearray = []
             el.find(".arya-slides section").each(function (index, elem) {
                 slidearray.push(elem);
-                console.log(index);
+                
             });
             //step 2 : hide arya-slide (done in css
             //el.find(".arya-slides").hide();
             //step 3 : show/prepare arya content div
             var contentel = el.find(".arya-content")
             $(contentel).css({
-//                width: "300px",
-//                height: "auto",
+                //                width: "300px",
+                //                height: "auto",
             })
             //step 4 lets try the slide and see
             var i = 1 // counter for slider
-            //hack
-           // run first slide while waiting for interval to kick in
-            $(contentel).html(slidearray[0])
+                //hack
+                // run first slide while waiting for interval to kick in
+            $("#page0").addClass("active");
+            $(contentel).html(slidearray[0]);
+            
             setInterval(
                 function () {
 
                     if (i < slidearray.length) {
+                        
                         //clear content 
                         $(contentel).html("")
-                       //Animations
+                        //Animations
                         //one
                         //$(contentel).fadeOut(500).html(slidearray[i]).fadeIn(1000);
                         //two
                         $(contentel).hide().html(slidearray[i]).slideDown(1000);
-                         i++;
+                        var k = i-1;
+                        $("#page"+k).removeClass("active");
+                        $("#page"+i).addClass("active");
+                        
+                        i++;
                     } else {
+                        var k = i-1;
+                        $("#page"+k).removeClass("active");
                         i = 0;
                     }
-                    
-                }, 5000)
+
+                }, 2000)
 
             //step 5 : Lets add some pagination
             //first lets create the div
-            
+            var $div = $("<div>", {
+                id: "arya-pagination",
+                class: "arya-pagination"
+            });
+            $div.click(function () { /* ... */ });
+            el.append($div);
+            //Now create page numbers div and append to paginationdiv
+        for(var k = 0;k < slidearray.length;k++){
+            var j = k+1
+            $("#arya-pagination").append('<a href="#" id="page'+k+'" class="page">'+j+'</a>')
+        }
+        
+        
         }
         //Options and settings
 
