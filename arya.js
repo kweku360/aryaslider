3b30a8963f9d1f9d80023f86b750adbc42ef8dfe
kweku okyere Kankam <kweku360@gmail.com>
2014-09-14 17:12:03 +0000

    Added options to configuration

--- arya.js
+++ arya.js
@@ -10,7 +10,7 @@ Version : 0.1
     //Private variables
 
     //Private methods
-    var slideshow = function (el) {
+    var slideshow = function (el, opt) {
             //this function returns the slides in an array
             //takes the element as a parameter
 
@@ -18,47 +18,47 @@ Version : 0.1
             var slidearray = []
             el.find(".arya-slides section").each(function (index, elem) {
                 slidearray.push(elem);
-                
+
             });
             //step 2 : hide arya-slide (done in css
-            //el.find(".arya-slides").hide();
+            el.css({
+                width: opt.width,
+                height: opt.height,
+            })
             //step 3 : show/prepare arya content div
             var contentel = el.find(".arya-content")
-            $(contentel).css({
-                //                width: "300px",
-                //                height: "auto",
-            })
+
             //step 4 lets try the slide and see
             var i = 1 // counter for slider
                 //hack
                 // run first slide while waiting for interval to kick in
             $("#page0").addClass("active");
             $(contentel).html(slidearray[0]);
-            
+
             setInterval(
                 function () {
 
                     if (i < slidearray.length) {
-                        
+
                         //clear content 
                         $(contentel).html("")
                         //Animations
                         //one
                         //$(contentel).fadeOut(500).html(slidearray[i]).fadeIn(1000);
                         //two
-                        $(contentel).hide().html(slidearray[i]).slideDown(1000);
-                        var k = i-1;
-                        $("#page"+k).removeClass("active");
-                        $("#page"+i).addClass("active");
-                        
+                        $(contentel).hide().html(slidearray[i]).slideDown(opt.slidespeed);
+                        var k = i - 1;
+                        $("#page" + k).removeClass("active");
+                        $("#page" + i).addClass("active");
+
                         i++;
                     } else {
-                        var k = i-1;
-                        $("#page"+k).removeClass("active");
+                        var k = i - 1;
+                        $("#page" + k).removeClass("active");
                         i = 0;
                     }
 
-                }, 2000)
+                }, opt.slideduration)
 
             //step 5 : Lets add some pagination
             //first lets create the div
@@ -69,18 +69,28 @@ Version : 0.1
             $div.click(function () { /* ... */ });
             el.append($div);
             //Now create page numbers div and append to paginationdiv
-        for(var k = 0;k < slidearray.length;k++){
-            var j = k+1
-            $("#arya-pagination").append('<a href="#" id="page'+k+'" class="page">'+j+'</a>')
-        }
-        
-        
+            for (var k = 0; k < slidearray.length; k++) {
+                var j = k + 1
+                $("#arya-pagination").append('<a href="#" id="page' + k + '" class="page">' + j + '</a>')
+            }
+
+
         }
         //Options and settings
+    var defaultoptions = {
+        width: "800px", //width of slider
+        height: "600px",//height of slider
+        slideduration : 1000,//time for each slide (in milliseconds)
+        slidespeed : 500, // slide transition speed(in milliseconds)
+        transitioneffect : "fadeIn",//current options : fadein,slideup,slidedown(more work needed here)
+    }
 
     //Main function
-    $.fn.arya = function () {
-        slideshow(this);
+    $.fn.arya = function (options) {
+
+        var settings = $.extend(defaultoptions, options)
+
+        slideshow(this, settings);
     };
 
 })(jQuery)
